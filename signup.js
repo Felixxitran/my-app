852f20548fe9f40f09cc97563514dec553051b15
Phan Kiệt <35695324+ketphan02@users.noreply.github.com>
2021-06-09 16:04:29 +0700

    Fix bug 1

--- signup.js
+++ signup.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import './signup.scss';
-import './filejs';
+import * as fileJS from './filejs';
+
 function Sign(){
+	
+	useEffect(() => {
+		console.log(fileJS);
+		const script = document.createElement('script');
+		script.src = fileJS;
+		script.async = true;
+		document.body.appendChild(script);
+		return () => {
+			document.body.removeChild(script);
+		}
+	}, []);
 
 	return(
 		<><><div class="cont s--inactive">
@@ -119,7 +131,7 @@ function Sign(){
 				<img src="http://icons.iconarchive.com/icons/uiconstock/socialmedia/256/Dribbble-icon.png" />
 			</a><a href="https://twitter.com/NikolayTalanov" target="_blank" class="icon-link icon-link--twitter">
 				<img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/twitter-128.png" />
-			</a></><script src="./filejs.js"></script></>
+			</a></></>
 	)
 }
 
